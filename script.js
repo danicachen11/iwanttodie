825c7075bce6bbd69fbff7024cfcab635fb47606
danicachen11 <144848748+danicachen11@users.noreply.github.com>
2025-11-26 03:11:02 -0600

    update

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 // Activate Hamburger button
-document.addEventListener('DOMContentLoaded', () => {
-  const hamburger = document.querySelector('.hamburger');
-  const mobileMenu = document.querySelector('.mobile-menu');
+const hamburger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-  hamburger.addEventListener('click', () => {
-    mobileMenu.classList.toggle('active');
+if (hamburger && mobileMenu) {
+	hamburger.addEventListener('click', () => {
+  	mobileMenu.classList.toggle('active');
   });
-});
+}
 
-// Submitting Newsletter form
+
+
+// "Book A Vist" Button Modal
+const openModal = document.getElementById('openModal');
+const modal = document.getElementById('modal');
+const closeModal = document.getElementById('closeModal');
+
+if (modal && openModal && closeModal) {
+	openModal.onclick = () => modal.classList.add('show');
+	closeModal.onclick = () => modal.classList.remove('show');
+
+	modal.addEventListener('click', (e) => {
+  	if(e.target === modal) modal.classList.remove('show');
+	});
+}
+
+
+
+
+// Submitting Newsletter Form
 const newsletterForm = document.getElementById('newsletterForm');
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
@@ -16,17 +35,20 @@ const formMessage = document.getElementById('formMessage');
 
 newsletterForm.addEventListener('submit', e => {
   e.preventDefault();
-
   //validations
+  // if either name or email not valid
   if (nameInput.value.trim() === "" || emailInput.value.trim() === "") {
     formMessage.textContent = "Please enter both name and email.";
     formMessage.style.color = "red";
   } else if (!emailInput.value.includes("@")) {
     formMessage.textContent = "Please enter a valid email.";
     formMessage.style.color = "red";
+    //show if valid input
   } else {
     formMessage.textContent = "Thank you for subscribing!";
     formMessage.style.color = "green";
     newsletterForm.reset();
   }
 });
+
+
